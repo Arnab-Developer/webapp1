719ff7917396e0eabbf0f986f7ef7990f6a6b904
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-02-05 11:37:36 +0530

    initial commit

---
+++ src/WebApp1.Ui/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApp1.Ui.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
